d1f0d05ccf392dac0b6676d499637ee6e5377914
bfa1trung <bfa1trung@yahoo.com>
2024-01-27 21:21:36 +0100

    final

---
+++ poms/jobs/process/websoap_0.1/src/main/templates/Job_run_template.bat
@@ -0,0 +1,3 @@
+%~d0
+cd %~dp0
+java -Dtalend.component.manager.m2.repository="%cd%/../lib" -Xms256M -Xmx1024M -Dfile.encoding=UTF-8 -cp .;../lib/routines.jar;../lib/log4j-slf4j-impl-2.13.2.jar;../lib/log4j-api-2.13.2.jar;../lib/log4j-core-2.13.2.jar;../lib/activation.jar;../lib/stax-ex-1.8.3.jar;../lib/jboss-marshalling-2.0.12.Final.jar;../lib/commons-codec-1.14.jar;../lib/saaj-impl-1.5.2.jar;../lib/xercesImpl-2.12.0.jar;../lib/jakarta.xml.soap-api-1.4.2.jar;../lib/dom4j-2.1.3.jar;../lib/slf4j-api-1.7.29.jar;../lib/xml-apis-1.4.01.jar;../lib/components-soap-2.3-20201016.jar;../lib/crypto-utils-0.31.12.jar;websoap_0_1.jar; tp1.websoap_0_1.WebSoap %*
---
+++ poms/jobs/process/webvelo_list_ville_0.1/src/main/templates/Job_run_template.ps1
@@ -0,0 +1,3 @@
+$fileDir = Split-Path -Parent $MyInvocation.MyCommand.Path
+cd $fileDir
+java '-Dtalend.component.manager.m2.repository=%cd%/../lib' '-Xms256M' '-Xmx1024M' '-Dfile.encoding=UTF-8' -cp '.;../lib/routines.jar;../lib/log4j-slf4j-impl-2.13.2.jar;../lib/log4j-api-2.13.2.jar;../lib/log4j-core-2.13.2.jar;../lib/jsr311-api-1.1.1.jar;../lib/jboss-marshalling-2.0.12.Final.jar;../lib/jersey-client-1.19.jar;../lib/talend_file_enhanced-1.1.jar;../lib/jersey-core-1.19.jar;../lib/dom4j-2.1.3.jar;../lib/slf4j-api-1.7.29.jar;../lib/talendcsv-1.0.0.jar;../lib/crypto-utils-0.31.12.jar;webvelo_list_ville_0_1.jar;' tp1.webvelo_list_ville_0_1.WebVelo_List_Ville $args
